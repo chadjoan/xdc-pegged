5bd57217bb93910b4deb9942e3fda4222d2d2741
cjoan <chadjoan@gmail.com>
2019-05-12 18:20:18 -0400

    I'm not sure why these are changed.
    This might just be bumping the version of Pegged used by XDC
    (if this is used by XDC at all right now).

--- test.d
+++ test.d
@@ -25,8 +25,19 @@ mixin(grammar(`Test:
     A <{log, log} 'a' { log }
     `));
 
+mixin(grammar(
+`
+Palindromes:
+A <- '0' B '0' / '1' B '1'
+B <- A / '00' / '11' / '0' / '1'
+`));
+
 void main()
 {
     writeln(Test("abc"));
+    writeln(Palindromes.A("0110"));
+    writeln(Palindromes.A("0000001000000"));
+    writeln(Palindromes.A("00000010000000"));
+    writeln(Palindromes.A("000001100000"));
 }
 
